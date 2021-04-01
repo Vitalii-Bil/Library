ebb5dc8fb1c7803967066519fcebd206424899b5
Your Name <you@example.com>
2021-04-01 14:56:22 +0300

    flake8 fixes, need to fix signal'

--- lib_employee/wharehouse/signals.py
+++ lib_employee/wharehouse/signals.py
@@ -1,15 +1,14 @@
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from .models import Order
-
-from django.core.mail import send_mail
 
 
 @receiver(post_save, sender=Order)
 def do_something_when_user_updated(sender, instance, created, **kwargs):
     if not created:
-        order_obj = instance
         subject = 'Test'
-        message = f'Test'
+        message = 'Test'
         from_email = 'ad@ex.com'
         send_mail(subject, message, from_email, ['admin@example.com'])
